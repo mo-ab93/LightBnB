102a0a86eaf0c07c65dabc89f9fa04b1b9f032b6
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-08-25 15:51:03 -0400

    update getAllReservations function and add query to it

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -62,7 +62,7 @@ exports.getUserWithId = getUserWithId;
  */
 const addUser =  function(user) {
   return pool
-  .query(`INSERT INTO users (name,email, password) VALUES($1, $2, $3)
+  .query(`INSERT INTO users (name, email, password) VALUES($1, $2, $3)
   RETURNING *;`, [user.name, user.email, user.password])
   .then((user) => {
     console.log(user.rows[0]);
@@ -82,7 +82,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then((user) => {
+    console.log(user.rows[0]);
+    return user.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
