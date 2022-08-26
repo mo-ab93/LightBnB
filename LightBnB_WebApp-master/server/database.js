768c92527ba87e35ed077c697c2f327298cbcf8c
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-08-25 21:10:03 -0400

    update addProperty Function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -123,10 +123,12 @@ exports.getAllReservations = getAllReservations;
     queryParams.push(`%${options.city}%`);
     queryString += `WHERE city LIKE $${queryParams.length} `;
   }
+
   if (options.owner_id) {
     queryParams.push(options.owner_id);
     queryString += `WHERE owner_id = $${queryParams.length} `;
   }
+
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     const minCost = Math.ceil(options.minimum_price_per_night * 100);
     const maxCost = Math.ceil(options.maximum_price_per_night * 100);
@@ -169,9 +171,32 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+   return pool
+  .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url,
+    cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms,
+    country, street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING * ;`, [property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    property.country,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,])
+
+  .then((res) => {
+    console.log(res.rows[0]);
+    return res.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.addProperty = addProperty;
